9b81afdcf1392ce42f39ddd1e1093aed663dbee4
hailu <hailuozhou@gmail.com>
2024-07-12 01:16:48 -0500

    Add feature

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+//this is learning purpose
+// this is for testing purpose
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline!!!!.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
